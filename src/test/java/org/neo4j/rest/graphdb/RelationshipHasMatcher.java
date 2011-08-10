8fe9f81981fdf55cdf8443895124da58c01f980e
Klemens Burchardi <kb@jnamic.com>
2011-08-10 18:25:15 +0200

    * wrote tests for some new restnode methods that were implemented to the bump to neo 1.4  * wrote a corresponding matcher for the tests

---
+++ src/test/java/org/neo4j/rest/graphdb/RelationshipHasMatcher.java
@@ -0,0 +1,71 @@
+package org.neo4j.rest.graphdb;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.Description;
+import org.junit.internal.matchers.TypeSafeMatcher;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+
+public class RelationshipHasMatcher extends TypeSafeMatcher<Iterable<Relationship>>{
+	
+	private final Node node;
+	private final Direction direction;
+	private final List<String> typeNames;
+
+	public RelationshipHasMatcher(Node startNode, Direction direction, RelationshipType... types){
+		this.node = startNode;
+		this.direction = direction;		
+		this.typeNames = fillTypeNames(Arrays.asList(types));
+	}
+	
+	
+	
+	@Override
+	public void describeTo(Description description) {
+		description.appendText("Not all relationships matched the constraints. Node: ").appendValue(node).appendText(" direction: ").appendValue(direction).appendText(" relationship type(s): ").appendValue(typeNames);
+		
+	}
+
+	@Override
+	public boolean matchesSafely(Iterable<Relationship> relationships) {
+		for (Relationship relationship : relationships) {
+			
+			boolean isStartnode = this.node.equals(relationship.getStartNode());
+			boolean isEndnode = this.node.equals(relationship.getEndNode());
+			if (!isStartnode && !isEndnode){
+				return false;
+			}
+			
+			Direction relationshipDirection = isStartnode ? Direction.OUTGOING : Direction.INCOMING;
+			
+			if (this.direction != null && this.direction!= relationshipDirection){
+				return false;
+			}
+						
+						
+			if(!this.typeNames.isEmpty() && !this.typeNames.contains(relationship.getType().name())){				
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public static RelationshipHasMatcher match(Node startNode, Direction direction, RelationshipType... types){
+		return new RelationshipHasMatcher(startNode, direction, types);
+	}
+	
+	public static List<String> fillTypeNames(List<RelationshipType> tmptypes){
+		List<String> tmpNames = new ArrayList<String>();
+		for (RelationshipType type : tmptypes) {
+			tmpNames.add(type.name());
+		}
+		return tmpNames;
+	}
+
+}
