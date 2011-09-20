6f559e13667b4b593da5976202a1bfada91fe58d
Klemens Burchardi <kb@jnamic.com>
2011-09-20 17:32:50 +0200

    * used new RecordingRestOperations constructor

--- src/test/java/org/neo4j/rest/graphdb/RecordingRestRequestTest.java
+++ src/test/java/org/neo4j/rest/graphdb/RecordingRestRequestTest.java
@@ -11,22 +11,18 @@ public class RecordingRestRequestTest {
     
     private ExecutingRestRequest exeRequest;
     private RecordingRestRequest recRequest;
-    private URI baseUri;
+    private String baseUri;
     
     @Before
-    public void init(){
-        try {
-            this.baseUri = new URI("www.test.net");
+    public void init(){        
+            this.baseUri = "www.test.net";
             this.exeRequest = new ExecutingRestRequest(baseUri);
-            this.recRequest = new RecordingRestRequest(exeRequest);
-        } catch (URISyntaxException e) {
-            
-        }
+            this.recRequest = new RecordingRestRequest(exeRequest, new RestOperations());       
     }
     
     @Test
     public void testCreate() {
-        RecordingRestRequest testRequest = new RecordingRestRequest(exeRequest);
+        RecordingRestRequest testRequest = new RecordingRestRequest(exeRequest, new RestOperations());
         assertEquals(this.baseUri, testRequest.baseUri);
         assertEquals(0, testRequest.getRecordedRequests().size());
         
@@ -35,48 +31,48 @@ public class RecordingRestRequestTest {
     @Test
     public void testGetWithoutData(){
        RequestResult response = recRequest.get("/node");      
-       assertEquals(0, response.getBatchId());
+       assertEquals(1, response.getBatchId());
        assertEquals(1, recRequest.getRecordedRequests().size());
     }
     
     @Test
     public void testGetWithData(){
        RequestResult response = recRequest.get("/node","Test");      
-       assertEquals(0, response.getBatchId());
+       assertEquals(1, response.getBatchId());
        assertEquals(1, recRequest.getRecordedRequests().size());
     }
     
     @Test
     public void testPost(){
        RequestResult response = recRequest.post("/node","Test");      
-       assertEquals(0, response.getBatchId());
+       assertEquals(1, response.getBatchId());
        assertEquals(1, recRequest.getRecordedRequests().size());
     }
     
     @Test
     public void testPut(){
        RequestResult response = recRequest.put("/node","Test");      
-       assertEquals(0, response.getBatchId());
+       assertEquals(1, response.getBatchId());
        assertEquals(1, recRequest.getRecordedRequests().size());
     }
     
     @Test
     public void testDelete(){
        RequestResult response = recRequest.delete("/node");      
-       assertEquals(0, response.getBatchId());
+       assertEquals(1, response.getBatchId());
        assertEquals(1, recRequest.getRecordedRequests().size());
     }
     
     @Test
     public void testMultipleEntries(){
        RequestResult response = recRequest.post("/node","Test");      
-       assertEquals(0, response.getBatchId());
+       assertEquals(1, response.getBatchId());
        assertEquals(1, recRequest.getRecordedRequests().size());     
        response = recRequest.delete("/node");      
-       assertEquals(1, response.getBatchId());
+       assertEquals(2, response.getBatchId());
        assertEquals(2, recRequest.getRecordedRequests().size());
        response = recRequest.get("/node","Test");      
-       assertEquals(2, response.getBatchId());
+       assertEquals(3, response.getBatchId());
        assertEquals(3, recRequest.getRecordedRequests().size());
     }
 
